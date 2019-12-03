5e53295aa2d6a81a6f7fc7e896e90d95b2c0b09f
olga_charna <olga_charna@192.168.0.112>
2019-12-03 21:32:21 +0100

    гуи расписание и даты

--- src/main/java/sk/upjs/paz/diary/gui/ScheduleController.java
+++ src/main/java/sk/upjs/paz/diary/gui/ScheduleController.java
@@ -41,6 +41,20 @@ public class ScheduleController extends Controller {
 	private Label monthLabel;
 
 	@FXML
+	private Label modayDateLabel;
+
+	@FXML
+	private Label tuesdayDateLabel;
+
+	@FXML
+	private Label wednesdayDateLabel;
+
+	@FXML
+	private Label thursdayDateLabel;
+
+	@FXML
+	private Label fridayDateLabel;
+	@FXML
 	private JFXButton removeSubjectButton;
 
 	@FXML
@@ -79,21 +93,70 @@ public class ScheduleController extends Controller {
 	 */
 	private void initMonthLabel() {
 		Month month = null;
+		int monday = 0;
+		int tuesday = 0;
+		int wednesday = 0;
+		int thursday = 0;
+		int friday = 0;
+		int week = 0;
 		if (!mondayListView.getItems().isEmpty()) {
 			month = mondayListView.getItems().get(0).getDateTime().getMonth();
+			monday = mondayListView.getItems().get(0).getDateTime().getDayOfMonth();
+			week = mondayListView.getItems().get(0).getDateTime().getDayOfMonth();
 		} else if (!tuesdayListView.getItems().isEmpty()) {
 			month = tuesdayListView.getItems().get(0).getDateTime().getMonth();
+			tuesday = tuesdayListView.getItems().get(0).getDateTime().getDayOfMonth();
 		} else if (!wednesdayListView.getItems().isEmpty()) {
 			month = wednesdayListView.getItems().get(0).getDateTime().getMonth();
+			wednesday = wednesdayListView.getItems().get(0).getDateTime().getDayOfMonth();
 		} else if (!thursdayListView.getItems().isEmpty()) {
 			month = thursdayListView.getItems().get(0).getDateTime().getMonth();
+			thursday = thursdayListView.getItems().get(0).getDateTime().getDayOfMonth();
 		} else if (!fridayListView.getItems().isEmpty()) {
 			month = fridayListView.getItems().get(0).getDateTime().getMonth();
+			friday = fridayListView.getItems().get(0).getDateTime().getDayOfMonth();
 		} else {
 			monthLabel.setText(null);
+			modayDateLabel.setText(null);
+			tuesdayDateLabel.setText(null);
+			wednesdayDateLabel.setText(null);
+			thursdayDateLabel.setText(null);
+			fridayDateLabel.setText(null);
 			return;
 		}
 		monthLabel.setText(month.toString());
+		if (monday != 0) {
+			modayDateLabel.setText(String.valueOf(monday));
+			tuesdayDateLabel.setText(String.valueOf(monday + 1));
+			wednesdayDateLabel.setText(String.valueOf(monday + 2));
+			thursdayDateLabel.setText(String.valueOf(monday + 3));
+			fridayDateLabel.setText(String.valueOf(monday + 4));
+		} else if (tuesday != 0) {
+			modayDateLabel.setText(String.valueOf(tuesday - 2));
+			tuesdayDateLabel.setText(String.valueOf(tuesday));
+			wednesdayDateLabel.setText(String.valueOf(tuesday + 1));
+			thursdayDateLabel.setText(String.valueOf(tuesday + 2));
+			fridayDateLabel.setText(String.valueOf(tuesday + 3));
+		} else if (wednesday != 0) {
+			modayDateLabel.setText(String.valueOf(wednesday - 2));
+			tuesdayDateLabel.setText(String.valueOf(wednesday - 1));
+			wednesdayDateLabel.setText(String.valueOf(wednesday));
+			thursdayDateLabel.setText(String.valueOf(wednesday + 1));
+			fridayDateLabel.setText(String.valueOf(wednesday + 2));
+		} else if (thursday != 0) {
+			modayDateLabel.setText(String.valueOf(thursday - 3));
+			tuesdayDateLabel.setText(String.valueOf(thursday - 2));
+			wednesdayDateLabel.setText(String.valueOf(thursday - 1));
+			thursdayDateLabel.setText(String.valueOf(thursday));
+			fridayDateLabel.setText(String.valueOf(thursday + 1));
+		} else if (friday != 0) {
+			modayDateLabel.setText(String.valueOf(friday - 4));
+			tuesdayDateLabel.setText(String.valueOf(friday - 3));
+			wednesdayDateLabel.setText(String.valueOf(friday - 2));
+			thursdayDateLabel.setText(String.valueOf(friday - 1));
+			fridayDateLabel.setText(String.valueOf(friday));
+		}
+
 	}
 
 	/**
@@ -116,12 +179,12 @@ public class ScheduleController extends Controller {
 		setItemsToSubjectsListView();
 	}
 
-	@FXML
-	void removeSubjectButtonClick(ActionEvent event) {
-		Subject sbj = subjectListView.getSelectionModel().getSelectedItem();
-		subjectListView.getItems().remove(sbj);
-		subjectDao.remove(sbj);
-		setOrRefreshItemsToLessonsListViews();
-	}
+//	@FXML
+//	void removeSubjectButtonClick(ActionEvent event) {
+//		Subject sbj = subjectListView.getSelectionModel().getSelectedItem();
+//		subjectListView.getItems().remove(sbj);
+//		subjectDao.remove(sbj);
+//		setOrRefreshItemsToLessonsListViews();
+//	}
 
 }
